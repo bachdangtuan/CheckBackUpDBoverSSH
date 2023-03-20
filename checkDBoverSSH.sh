3016713016e38ed7754d221d1b05cd9869230ece
DESKTOP-E9M843H\IS-00002 <bachdangtuan.dev@gmail.com>
2023-03-20 13:59:17 +0700

    no message

--- checkDBoverSSH.sh
+++ checkDBoverSSH.sh
@@ -4,9 +4,18 @@
 
 # Variable
 SERVER_IP='10.0.0.121'
-COMMAND='
-    echo "thành công"
 
-'
+variable_name() {
+  # Body of the function
+  # ...
+  echo "thanh cong"
+}
 
-ssh $SERVER_IP $COMMAND
+# Create a script that defines the function and calls it
+echo 'function_to_call=$(declare -f variable_name); eval "$function_to_call"; variable_name' > myscript.sh
+
+# Make the script executable
+chmod +x myscript.sh
+
+# Call the script using SSH
+ssh "$SERVER_IP" "./myscript.sh"
