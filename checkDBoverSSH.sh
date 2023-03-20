7a1163a323d3fa9ad3b5fbe42be1d60449e63f35
DESKTOP-V258CSQ\DarkTuan <bachdangtuan93@gmail.com>
2023-03-20 22:26:17 +0700

    no message

--- checkDBoverSSH.sh
+++ checkDBoverSSH.sh
@@ -15,28 +15,30 @@ ketqua=$(ssh "$SERVER_IP" "bash /root/scriptCheck/myscript.sh")
 readarray -t result_array <<< "$ketqua"
 
 
-echo "$ketqua"
+# echo "$ketqua"
 
-# In kết quả
-#echo "Ket qua: $result_var"
-echo "IP la: ${result_array[0]}"
-echo "Server la: ${result_array[1]}"
-echo "PathBackUp: ${result_array[2]}"
-echo "Filename: ${result_array[3]}"
+# # In kết quả
+# #echo "Ket qua: $result_var"
+# echo "IP la: ${result_array[0]}"
+# echo "Server la: ${result_array[1]}"
+# echo "PathBackUp: ${result_array[2]}"
+# echo "Filename: ${result_array[3]}"
 
 
 # Check alert 
 filename=${result_array[3]}
 filedate=${filename:11:10}   # lấy chuỗi ngày/tháng/năm từ tên file
 today=$(date +%Y_%m_%d)      # lấy ngày hiện tại
-
+nameDatabase=${filename:0:10}
 
 echo $filedate
+echo $nameDatabase
+
 
 if [ "$filedate" == "$today" ]; then
-  echo "The file was created today"
+  alertTelegramSuccess
 else
-  echo "The file was not created today"
+  alertTelegramError
 fi
 
 
@@ -44,8 +46,18 @@ SUCCESS="
 ==[BACKUP-SUCCESS]==
 Server: ${result_array[1]}
 Database: ${DB_NAME}
-Address IP : ${result_array[0]} / 24
+Address IP :${result_array[0]}/24
 Nguyen nhan: Backup Dump thanh cong databases !
+"
+
+ERROR="
+==[BACKUP-ERROR]==
+Server: ${result_array[1]}
+Database: ${DB_NAME}
+Address IP :${result_array[0]}/24
+Content: Backup backup du lieu khong thanh cong !
+--------
+Nguyen nhan: Backup DB backup bi ngat giua chung, quyen truy cap sai, khong co db, chua backup
 "
 
 
@@ -55,4 +67,10 @@ curl -s -X POST $URL \
 --data-urlencode "text=$SUCCESS" \
 -d "parse_mode=HTML"
 }
-#alertTelegramSuccess
+
+alertTelegramError(){
+curl -s -X POST $URL \
+-G -d chat_id=$CHAT_ID \
+--data-urlencode "text=$ERROR" \
+-d "parse_mode=HTML"
+}
